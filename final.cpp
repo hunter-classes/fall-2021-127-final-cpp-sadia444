273108510104fbe21fac7211fe6c0e5f6bdc99a9
sadia444 <83612520+sadia444@users.noreply.github.com>
2021-12-16 06:02:26 +0000

    I updated final cpp

---
+++ final.cpp
@@ -0,0 +1,32 @@
+
+## Question 1
+void isRightTrianlge(int a, int b, int c)
+
+if((a*a + b*b = c*c))
+{
+std::cout << "True" << std::endl;
+} 
+  else {
+    std::cout << "True" << std::endl
+  }
+ }
+
+
+## Question 2
+
+void fizzbuzz(int i) {
+  for(i = 1 ; i <=15; i++)
+  {
+    if (i % 15 == 0){
+      std :: cout << "FizzBuzz" << endl;
+    }
+    else if(i % 3 == 0){
+      std :: cout << "Fuzz" << endl;{
+      
+      else if (i % 5 == 0){
+      std :: cout << "Buzz" << endl;
+    }
+  
+  }
+
+}
